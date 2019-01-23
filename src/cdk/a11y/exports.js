22c0de345a939bbd3a19efed0d841a945c76ae2b
Ray Roman <r.roman681@me.com>
2019-01-23 16:43:23 +0100

    CDK: add list key provider/consumer to exports file

--- src/cdk/a11y/exports.js
+++ src/cdk/a11y/exports.js
@@ -1,5 +1,5 @@
 export { default as FocusMonitor } from './focus-monitor/FocusMonitor';
 export * from './focus-monitor/context/FocusMonitorContext';
 
-export { default as ListKeyManager } from './key-manager/ListKeyManager';
+export { default as ListKeyManager, withListKeyConsumer, withListKeyProvider } from './key-manager/ListKeyManager';
 export * from './key-manager/context/ListKeyManagerContext';
