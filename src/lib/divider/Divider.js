c3b88b5ce646969df0de87e6fa22e08f103413e0
Ray Roman <r.roman681@me.com>
2019-01-26 11:19:17 +0100

    Divider: remove unused lodash import

--- src/lib/divider/Divider.js
+++ src/lib/divider/Divider.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import DividerRoot from './styles';
 
 function Divider({ vertical, inset, ...restProps }) {
