f0b58de712d2e3e3fddf4ad0b58883cc23400c94
Ray Roman <r.roman681@me.com>
2019-01-30 12:20:59 +0100

    CDK: remove unused viewport-ruler function

--- src/cdk/scrolling/viewport-ruler.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-/** Returns the viewport's width and height. */
-export function getViewportSize() {
-  if (_.isUndefined(window)) {
-    return { width: 0, height: 0 };
-  }
-  
-  return { width: window.innerWidth, height: window.innerHeight };
-}
