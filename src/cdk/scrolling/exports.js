f7437e871620e967a094836a65a47f93528b56da
Ray Roman <r.roman681@me.com>
2019-02-01 09:56:12 +0100

    CDK: fix scrolling exports

--- src/cdk/scrolling/exports.js
+++ src/cdk/scrolling/exports.js
@@ -1,8 +1,11 @@
 export {
-  default as ScrollDispatcher, withScrollDispatcher, ScrollDispatcherProvider,
-  ScrollDispatcherConsumer, ScrollDispatcherDefaultProps, ScrollDispatcherPropTypes,
+  default as ScrollDispatcher,
   DEFAULT_SCROLL_TIME,
 } from './ScrollDispatcher';
+export {
+  withScrollDispatcher, ScrollDispatcherProvider,
+  ScrollDispatcherConsumer, ScrollDispatcherDefaultProps, ScrollDispatcherPropTypes,
+} from './context/ScrollDispatcherContext';
 export {
   default as ViewportRuler,
   DEFAULT_RESIZE_TIME
