783fce7666d4ca2f56e5cfbea7c1c7c3b825c5f2
Ray Roman <r.roman681@me.com>
2019-02-01 12:46:28 +0100

    FormField: refactor main design to closely match Mat design

--- src/lib/form-field/FormField.js
+++ src/lib/form-field/FormField.js
@@ -7,11 +7,12 @@ import {
   FormFieldFlex, FormFieldHintSpacer, FormFieldHintWrapper, FormFieldInfix, FormFieldLabel, FormFieldLabelWrapper,
   FormFieldRequiredMarker, FormFieldRoot,
   FormFieldSubscriptWrapper,
-  FormFieldUnderline,
   FormFieldWrapper,
 } from './styles/index';
 import { Provider as FormFieldControlProvider } from './context/UIMachineContext';
 import { byInternalType } from '../core/components/util';
+
+const toArray = React.Children.toArray;
 
 export default class FormField extends React.Component {
   constructor() {
@@ -45,71 +46,63 @@ export default class FormField extends React.Component {
         { ui: nextUiState.value };
     });
   };
-  
-  /*
-  Derived data
+
+  /**
+   * Refs
    */
-  // has a label prop
-  hasLabel = () => !!this.props.label;
-  // has a placeholder prop
+  /** The connection container to bind the autocomplete component */
+  getConnectionContainer = (flex) => {
+    this.CONNECTION_CONTAINER = flex;
+  };
+
+  /**
+   * Derived data
+   */
+  /** Get the non-label, non-hint children */
+  getFlatChildren = () => toArray(this.props.children)
+    .filter(_.negate(byInternalType(
+      'Hint', 'Label', 'Error', 'Prefix', 'Suffix',
+    )));
+
+  /** Filter children by */
+  getFilteredChildrenBy = (...types) => () => toArray(this.props.children)
+    .filter(byInternalType(...types));
+
+  /** Get the <Hint /> children */
+  getHints = this.getFilteredChildrenBy('Hint');
+
+  /** Get the <Error /> children */
+  getErrors = this.getFilteredChildrenBy('Error');
+
+  /** Get the <*Fix /> children */
+  getPrefix = this.getFilteredChildrenBy('Prefix');
+  getSuffix = this.getFilteredChildrenBy('Suffix');
+
+  /** Get the <Label /> child (only want one) */
+  getLabel = () => _.head(toArray(this.props.children)
+    .filter(byInternalType('Label')));
+
+  /** Has label children  */
+  hasLabel = () => !_.isNil(this.getLabel());
+
+  /** Has placeholder in FormFieldControl */
   hasPlaceholder = () => !!this.state.placeholder;
-  // has a label or placeholder (if in underline)
-  hasFloatingLabel = () => this.hasLabel() || this.props.appearance === 'underline' && this.hasPlaceholder();
-  // hiding the required marker
+
+  /** hiding the required marker */
   hideRequiredMarker = () => this.props.hideRequiredMarker;
-  // hide the placeholder
-  hidePlaceholder = () => (
-    (this.props.appearance === 'legacy' && !this.hasLabel()) ||
-    (this.hasLabel() && _.stubFalse)
-  );
-  // is required, disabled, etc.
+
+  /** is required, disabled, etc. */
   isFocused = () => _.get(this.state.ui, 'field.enabled') === 'focused';
   isFilled = () => _.get(this.state.ui, 'value') === 'filled';
   isDisabled = () => _.get(this.state.ui, 'field') === 'disabled';
   isRequired = () => _.get(this.state.ui, 'required-status') === 'required';
-  // messages
+
+  /** messages */
   getDisplayedMessages = () => this.props.errorState ? 'error' : 'hint';
-  // children
-  getFlatChildren = () => {
-    return React.Children.toArray(this.props.children)
-      .filter(_.negate(byInternalType('Hint')));
-  };
   
   /*
-  Refs
+  Actions
    */
-  getFlexRef = (flex) => {
-    this.FLEX_REF = flex;
-  };
-  
-  getHintWrapperRef = (hint) => {
-    this.HINT_WRAPPER_REF = hint;
-  };
-  
-  /*
-  Handling actions
-   */
-  onFocusCapture = () => {
-    this.transition('FOCUS');
-  };
-  
-  onBlurCapture = () => {
-    if (!_.isUndefined(window)) {
-      window.setTimeout(() => {
-        /*
-        We have to wait until the NEW focus event is complete. If there are
-        multiple focusable elements in the form field, we don't want to transition
-        back to FOCUS again if we blur temporarily. We do that by querying if the
-        current active element is still located in the form field. If it is,
-        then we don't bother with blurring.
-         */
-        if (!this.FLEX_REF.contains(document.activeElement)) {
-          this.transition('BLUR');
-        }
-      });
-    }
-  };
-  
   changeDescribedByIds = ({ added = null, removed = null }) => {
     this.setState((state) => {
       let ids = state.describedByIds;
@@ -122,24 +115,44 @@ export default class FormField extends React.Component {
       return { describedByIds: ids };
     });
   };
-  
-  /*
-  Rendering
+
+  /**
+   * Renderers
    */
-  // rendering the part below the field
+  /*
+  Rendering the label
+
+  This is separately rendered because the transform/positioning of the label is different
+  depending on a prefix icon---if it were placed as a child of Infix, then it would only float
+  above the infix, which would look strange in an outline + prefix combo.
+  */
+  renderLabel = () => (
+    <FormFieldLabelWrapper>
+      <FormFieldLabel
+        id={`${this.state.id}--label`}
+        htmlFor={this.state.id}
+        aria-owns={this.state.id}
+      >
+        { this.hasLabel() ? this.getLabel() : this.state.placeholder }
+        { !this.hideRequiredMarker() && this.isRequired() && !this.isDisabled() ? (
+          <FormFieldRequiredMarker>&nbsp;*</FormFieldRequiredMarker>
+        ) : null }
+      </FormFieldLabel>
+    </FormFieldLabelWrapper>
+  );
+  /** The hints/errors underneath the main outline */
   renderSubscript = () => {
-    const children = React.Children.toArray(this.props.children);
     const messageType = this.getDisplayedMessages();
     if (messageType === 'error') {
       // only return the first error
-      return children.find(child => _.get(child.props, '__sui-internal-type') === 'Error');
+      return this.getErrors();
     }
     
     if (messageType === 'hint') {
       // we may have hints (more than one) with align={start} or align={end}
-      const hints = children.filter(child => _.get(child.props, '__sui-internal-type') === 'Hint');
+      const hints = this.getHints();
       return (
-        <FormFieldHintWrapper ref={this.getHintWrapperRef}>
+        <FormFieldHintWrapper>
           { hints.find(child => _.get(child.props, 'align') === 'start') }
           <FormFieldHintSpacer />
           { hints.find(child => _.get(child.props, 'align') === 'end') }
@@ -150,36 +163,14 @@ export default class FormField extends React.Component {
     return null;
   };
   
-  // rendering the label
-  /*
-    This is separately rendered because the transform/positioning of the label is different
-    depending on a prefix icon---if it were placed as a child of Infix, then it would only float
-    above the infix, which would look strange in an outline + prefix combo.
-  */
-  renderLabel = () => (
-    this.hasFloatingLabel() ? (
-      <FormFieldLabelWrapper>
-        <FormFieldLabel
-          id={`${this.state.id}--label`}
-          htmlFor={this.state.id}
-          aria-owns={this.state.id}
-        >
-          { !this.hasLabel() ? this.state.placeholder : this.props.label }
-          { !this.hideRequiredMarker() && this.isRequired() && !this.isDisabled() ? (
-            <FormFieldRequiredMarker>&nbsp;*</FormFieldRequiredMarker>
-          ) : null }
-        </FormFieldLabel>
-      </FormFieldLabelWrapper>
-    ) : null
-  );
-  
   render() {
+    const prefix = this.getPrefix();
+    const suffix = this.getSuffix();
     return (
       <FormFieldRoot
         {...this.props}
         data-appearance={this.props.appearance}
         data-disabled={this.isDisabled().toString()}
-        data-noop-placeholder={this.hidePlaceholder().toString()}
         data-focus={this.isFocused() ? 'focused' : 'idle'}
         data-value={this.isFilled() ? 'filled' : 'empty'}
       >
@@ -188,28 +179,21 @@ export default class FormField extends React.Component {
           this.renderLabel() :
           null
         }
-        <FormFieldWrapper
-          onFocusCapture={this.onFocusCapture}
-          onBlurCapture={this.onBlurCapture}
-        >
+        <FormFieldWrapper>
           <FormFieldControlProvider value={this.state}>
             <FormFieldFlex
               onClick={this.state.containerClick}
-              innerRef={this.getFlexRef}
+              innerRef={this.getConnectionContainer}
             >
-              { this.props.prefix ? <FormFieldFix>{ this.props.prefix }</FormFieldFix> : null }
+              { prefix ? <FormFieldFix>{ prefix }</FormFieldFix> : null }
               <FormFieldInfix>
                 { // don't pass through the hints, otherwise it'll show up twice
                   this.getFlatChildren()
                 }
                 { this.props.appearance !== 'outline' ? this.renderLabel() : null }
               </FormFieldInfix>
-              { this.props.suffix ? <FormFieldFix>{ this.props.suffix }</FormFieldFix> : null }
+              { suffix ? <FormFieldFix>{ suffix }</FormFieldFix> : null }
             </FormFieldFlex>
-            { this.props.appearance !== 'outline' ? (
-              // underline used for non-outline appearances
-              <FormFieldUnderline />
-            ) : null }
             <FormFieldSubscriptWrapper>
               { this.renderSubscript() }
             </FormFieldSubscriptWrapper>
@@ -221,15 +205,17 @@ export default class FormField extends React.Component {
 }
 
 FormField.propTypes = {
-  appearance: PropTypes.oneOf(['outline', 'fill', 'underline']),
-  color: PropTypes.oneOf(['primary', 'accent', 'warn', 'light', 'dark']),
-  label: PropTypes.node,
+  /** The appearance */
+  appearance: PropTypes.oneOf(['standard', 'fill']),
 };
 
 FormField.defaultProps = {
-  // query the type so it can be filtered by Children.toArray().filter()
-  '__sui-internal-type': 'Form-Field',
+  /**
+   * Use a default internal prop here to allow other members to
+   * query by this type via:
+   * Children.toArray(this.props.children)
+   *   .filter(child => child.props['__sui-internal-type'] === 'FormField')
+   */
+  '__sui-internal-type': 'FormField',
   appearance: 'outline',
-  color: 'accent',
-  label: null,
 };
