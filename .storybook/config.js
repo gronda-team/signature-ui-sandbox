1dbcd87929109171d4b059bf052eadb438544db2
Ray Roman <r.roman681@me.com>
2019-02-01 11:05:22 +0100

    Storybook: add global context providers to config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,22 @@
-import { configure } from '@storybook/react';
+import * as React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import {Platform} from '../src/cdk/platform';
+import {OverlayContainer} from '../src/cdk/overlay';
+import {FocusMonitor} from '../src/cdk/a11y';
+
+/**
+ * Add the important global DI containers
+ * so that FocusMonitor works, OverlayContainer exists, etc.
+ */
+addDecorator(story => (
+  <Platform>
+    <OverlayContainer>
+      <FocusMonitor>
+        { story() }
+      </FocusMonitor>
+    </OverlayContainer>
+  </Platform>
+));
 
 /**
  * Dynamically load the stories from src/storybook-app folder,
