28e12322c679c0804edaec09a33a4b77a66e012e
Ray Roman <r.roman681@me.com>
2019-02-01 10:46:07 +0100

    Storybook: update folder name in config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@storybook/react';
 
 /**
- * Dynamically load the stories from src/dev folder,
+ * Dynamically load the stories from src/storybook-app folder,
  * and target files that have .stories.js in the filename.
  */
-const req = require.context('../src/dev', true, /\.stories\.js$/);
+const req = require.context('../src/storybook-app', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
