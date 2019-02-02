11079f3b3eab00ba5de7ed8d278663047d28f253
Ray Roman <r.roman681@me.com>
2019-02-02 21:17:34 +0100

    Storybook: add Autofilll story to main entry file

--- src/storybook-app/FormField.stories.js
+++ src/storybook-app/FormField.stories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FormField, Suffix, Prefix, Label, Hint } from '../lib/form-field';
+import { FormField, Suffix, Label, Hint } from '../lib/form-field';
 import { Input } from '../lib/input';
 import PrefixSuffix from './FormField/PrefixSuffix';
+import AutofillExample from './FormField/AutofillExample';
 
 storiesOf('FormField', module)
   .add('Available styles', () => (
@@ -32,4 +33,5 @@ storiesOf('FormField', module)
   ))
   .add('Prefixes and suffixes', () => (
     <PrefixSuffix />
-  ));
+  ))
+  .add('Autofill monitor example', () => <AutofillExample />);
