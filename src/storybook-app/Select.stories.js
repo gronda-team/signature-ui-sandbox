64a5c6c1a43ed417c3761ba7be47a125bc99f96e
Ray Roman <r.roman681@me.com>
2019-02-02 21:38:16 +0100

    Storybook: add Select storybook with Overview story

---
+++ src/storybook-app/Select.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Overview from './Select/Overview';
+
+storiesOf('Select', module)
+  .add('Overview', () => <Overview />);
