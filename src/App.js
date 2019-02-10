ed85690d6ca5ea3688d564250d7a02c5969d2135
Ray Roman <r.roman681@me.com>
2019-02-10 14:04:25 +0100

    Global: remove unused views/packages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import PasswordReset from './views/settings/PasswordReset';
-import UserSettings from './views/settings/UserSettings';
-import Details from './views/job/Details';
-import CreateStory from './views/story/CreateStory';
-import JobPanels from './views/job/JobPanels';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserSettings />
+        Hello world
       </div>
     );
   }
