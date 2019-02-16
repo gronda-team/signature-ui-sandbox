724e8a084bc98698671afc5060341916b4d382c5
Ray Roman <r.roman681@me.com>
2019-02-16 10:20:07 +0100

    CDK: update bidirectionality folder with prop types

--- src/cdk/bidi/constants.js
+++ src/cdk/bidi/constants.js
@@ -1,3 +1,23 @@
+import PropTypes from 'prop-types';
+
+/**
+ * Function that can be bound to class instances
+ * (like this.isRtl = isRtl.bind(this) in the
+ * constructor) to detect whether the current prop
+ * is an RTL context.
+ * @returns {boolean}
+ */
 export function isRtl() {
   return this.props.dir === 'rtl';
 }
+
+/**
+ * RTL prop types and default props
+ */
+export const RTLPropTypes = {
+  dir: PropTypes.oneOf(['ltr', 'rtl', 'auto']),
+};
+
+export const RTLDefaultProps = {
+  dir: 'ltr',
+};
