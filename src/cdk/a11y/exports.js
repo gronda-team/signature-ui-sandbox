79d428296bf8b15a97b69c4e4ddd8bf56cf8332e
Ray Roman <r.roman681@me.com>
2019-02-21 10:00:08 +0100

    Test: create focus trap test

--- src/cdk/a11y/exports.js
+++ src/cdk/a11y/exports.js
@@ -4,5 +4,7 @@ export * from './focus-monitor/context/FocusMonitorContext';
 export { default as InteractivityChecker } from './interactivity-checker/InteractivityChecker';
 export * from './interactivity-checker/context/InteractivityCheckerContext';
 
+export { default as FocusTrap } from './focus-trap/FocusTrap';
+
 export { default as ListKeyManager, withListKeyConsumer, withListKeyProvider } from './key-manager/ListKeyManager';
 export * from './key-manager/context/ListKeyManagerContext';
