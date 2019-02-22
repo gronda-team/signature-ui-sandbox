48865d1d71465128e7c61b5d201c7e0d75b7814e
Ray Roman <r.roman681@me.com>
2019-02-22 13:45:44 +0100

    Storybook: create dialog story

---
+++ src/storybook-app/Dialog.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import DialogExample from './Dialog/Overview';
+
+storiesOf('Autocomplete', module)
+  .add('Overview', () => <DialogExample />);
