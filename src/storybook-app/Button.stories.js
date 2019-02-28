286f451675de6d3660e42fb3bbd8d6cef0ee3228
Ray Roman <r.roman681@me.com>
2019-02-28 11:54:07 +0100

    Storybook: add full width button story

--- src/storybook-app/Button.stories.js
+++ src/storybook-app/Button.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '../lib/button';
 import ButtonOverview from './Button/Overview';
 import ButtonAnchorTag from './Button/AnchorTag';
+import ButtonFullWidth from './Button/FullWidth';
 import { CenteredDecorator } from './styles';
 
 storiesOf('Button', module)
   .addDecorator(story => <CenteredDecorator>{ story() }</CenteredDecorator>)
   .add('Overview', () => <ButtonOverview />)
-  .add('as <a /> tags', () => <ButtonAnchorTag />);
+  .add('as <a /> tags', () => <ButtonAnchorTag />)
+  .add('as full width', () => <ButtonFullWidth />);
