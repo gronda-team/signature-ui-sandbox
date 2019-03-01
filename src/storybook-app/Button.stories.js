2a93564cabed917e1ae845c73b799efc0800c031
Ray Roman <r.roman681@me.com>
2019-03-01 08:58:37 +0100

    Storybook: add inline form field story

--- src/storybook-app/Button.stories.js
+++ src/storybook-app/Button.stories.js
@@ -4,10 +4,12 @@ import { Button } from '../lib/button';
 import ButtonOverview from './Button/Overview';
 import ButtonAnchorTag from './Button/AnchorTag';
 import ButtonFullWidth from './Button/FullWidth';
+import ButtonInlineForm from './Button/InlineForm';
 import { CenteredDecorator } from './styles';
 
 storiesOf('Button', module)
   .addDecorator(story => <CenteredDecorator>{ story() }</CenteredDecorator>)
   .add('Overview', () => <ButtonOverview />)
   .add('as <a /> tags', () => <ButtonAnchorTag />)
-  .add('as full width', () => <ButtonFullWidth />);
+  .add('as full width', () => <ButtonFullWidth />)
+  .add('with inline form fields', () => <ButtonInlineForm />);
