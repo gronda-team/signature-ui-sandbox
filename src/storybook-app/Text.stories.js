a5a35bfb5b70f882860f04960426590b92f1f5f8
Ray Roman <r.roman681@me.com>
2019-03-09 18:23:33 +0100

    Storybook: add decorator to text stories

--- src/storybook-app/Text.stories.js
+++ src/storybook-app/Text.stories.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import TextOverview from './Text/Overview';
+import { CenteredDecorator } from './styles';
 
 storiesOf('Text', module)
+  .addDecorator(story => <CenteredDecorator>{ story() }</CenteredDecorator>)
   .add(
     'Overview',
     () => <TextOverview />,
