f0744ccb7248a760115e6fcd11d00cc6054a008d
Ray Roman <r.roman681@me.com>
2019-03-25 17:25:29 +0100

    Build: create rollup configuration file

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'prop-types',
+  ],
+};
