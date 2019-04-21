31bb34d997c7906af71d3c58646966e0653f2985
Ray Roman <r.roman681@me.com>
2019-04-21 16:10:04 +0200

    Core: update exports

--- src/lib/core/exports.js
+++ src/lib/core/exports.js
@@ -1,2 +1,4 @@
 export { default as SUIProvider } from './SUIProvider';
+export { generateTypographyConfig, generateTypographyLevel } from './theming/typography';
+export { generatePalette, generateThemeColors, getColor } from './theming/colors';
 export * from './option';
