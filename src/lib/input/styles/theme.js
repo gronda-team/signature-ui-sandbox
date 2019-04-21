80f1f8b7396473be90953a1c8ee3f928ac66354a
Ray Roman <r.roman681@me.com>
2019-04-21 17:09:30 +0200

    Input: update theming

--- src/lib/input/styles/theme.js
+++ src/lib/input/styles/theme.js
@@ -1,25 +1,39 @@
 import { css } from 'styled-components';
-import { GREY, GREEN, RED } from '../../../cdk/theme/colors';
+import _ from 'lodash';
+import { GREY } from '../../../cdk/theme/colors';
 import { PLACEHOLDER } from '../../core/styles/vendor';
 import { getLineHeight } from '../../text';
+import { DEFAULT_THEME_CONFIG } from '../../core/theming/colors';
+import { DEFAULT_TYPOGRAPHY_CONFIG } from '../../core/theming/typography';
 
-const active = GREEN[500];
-const warn = RED[500];
 const placeholderText = GREY[500];
 
-export const themeThunk = () => css`
-&:disabled {
-  color: ${placeholderText};
+/**
+ * These thunks don't require components, but to keep them standardized, we keep the functional
+ * declaration rather than exposing `themeThunkFromScProps` plainly.
+ */
+export function themeThunk() {
+  return function themeThunkFromScProps(props) {
+    const colors = _.get(props, 'theme.colors', DEFAULT_THEME_CONFIG);
+    const active = colors.primary.default;
+    return css`
+    &:disabled {
+      color: ${placeholderText};
+    }
+    caret-color: ${active};
+    ${PLACEHOLDER(`
+    color: ${placeholderText};
+    `)}`;
+  }
 }
-caret-color: ${active};
-${PLACEHOLDER(`
-color: ${placeholderText};
-`)}`;
 
-export const typographyThunk = (config) => {
-  const lineHeight = Number.parseFloat(getLineHeight(config, 'input'));
-  const lineSpacing = (lineHeight - 1) / 2;
-  return css`
-  margin-top: ${-lineSpacing}em;
-  `;
-};
+export function typographyThunk() {
+  return function typographyThunkFromScProps(props) {
+    const config = _.get(props, 'theme.typography', DEFAULT_TYPOGRAPHY_CONFIG);
+    const lineHeight = Number.parseFloat(getLineHeight(config, 'input'));
+    const lineSpacing = (lineHeight - 1) / 2;
+    return css`
+    margin-top: ${-lineSpacing}em;
+    `;
+  }
+}
