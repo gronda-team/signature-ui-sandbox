d9578e1830e72a7baf0d22b9596592c6894eee40
Ray Roman <r.roman681@me.com>
2019-05-13 10:10:38 +0200

    Exports: add theming + colors to exports

--- src/lib/core/exports.js
+++ src/lib/core/exports.js
@@ -1,4 +1,5 @@
 export { default as SUIProvider } from './SUIProvider';
-export { generateTypographyConfig, generateTypographyLevel } from './theming/typography';
-export { generatePalette, generateThemeColors, getColor } from './theming/colors';
+export { generateTypographyConfig, generateTypographyLevel, DEFAULT_TYPOGRAPHY_CONFIG } from './theming/typography';
+export { generatePalette, generateThemeColors, getColor, DEFAULT_THEME_CONFIG } from './theming/colors';
+export * from '../../cdk/theme/colors';
 export * from './option';
