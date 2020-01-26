b5cc120d6bbd9944d97fd8598724c89b3d6205cc
Ray Roman <r.roman681@me.com>
2020-01-26 13:07:24 +0100

    Storybook: add theming

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
+import { addons } from '@storybook/addons';
+import theme from './theme';
+
+addons.setConfig({ theme });
---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import '../assets/base.css';
+import { create } from '@storybook/theming/create';
+
+export default create({
+  // Typography
+  fontBase: '"TT Commons", sans-serif',
+  fontCode: 'monospace',
+});
