1c466d25359ace4005a4b03ed679828a4159ced6
Ray Roman <r.roman681@me.com>
2020-01-26 13:14:02 +0100

    Storybook: update splash page

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
+import { Centered } from './styles';
 
 export default {
   title: 'Welcome',
 };
 
-export const toStorybook = () => <Welcome showApp={linkTo('Button')} />;
+function SignatureUISplash() {
+  return (
+    <Centered data-size="large" data-sui-typography>
+      <h1>Welcome to Signature UI&nbsp;<sup>v3</sup></h1>
+      <p>Signature UI has several UI components ready to use.</p>
+      <p>Version 3 is currently in <b>active development</b>. Please check out some component demos
+        {' '}in the sidebar.</p>
+    </Centered>
+  );
+}
+
+export const toStorybook = () => <SignatureUISplash />;
 
 toStorybook.story = {
-  name: 'to Storybook',
+  name: 'About',
 };
